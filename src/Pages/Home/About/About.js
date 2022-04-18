58f1c0df720df9718ff17bd72cedb8f120a5b332
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-18 22:00:33 +0600

    about add

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-
 const About = () => {
     return (
-        <div>
-            <h3>This is about</h3>
-            <Footer></Footer>
+        <div style={{ height: "300px" }}>
+            <h1 className='text-center mt-3 fw-bold text-primary shadow'>ABOUT ME</h1>
+            <div className='d-flex justify-content-center mt-3'>
+                <div className='card p-2'>
+                    <img className='' src="https://i.ibb.co/HxgkZyM/dipu.png" alt="" />
+                    <h1>DIPANKAR HALDER</h1>
+                    <p>My feature goal is a good developer.It's my dream. At any cost I finish my dream</p>
+
+                </div>
+
+            </div>
+
         </div>
     );
 };
